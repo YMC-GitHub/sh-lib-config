cc0026e2d1265981bfe6c055a3053800f389dbe3
yemiancheng <ymc.github@gmail.com>
2019-11-17 16:42:25 +0800

    fix(core): fixing path_resovle with absolute path

--- dist/path-resolve.sh
+++ dist/path-resolve.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-function path_resolve(){
-str1="${1}"
-str2="${2}"
-slpit_char1=/
-slpit_char2=/
+function path_resolve_for_relative(){
+local str1="${1}"
+local str2="${2}"
+local slpit_char1=/
+local slpit_char2=/
 if [[ -n ${3} ]]
 then
     slpit_char1=${3}
@@ -15,8 +15,8 @@ then
 fi
 
 # 路径-转为数组
-arr1=(${str1//$slpit_char1/ }) 
-arr2=(${str2//$slpit_char2/ }) 
+local arr1=(${str1//$slpit_char1/ }) 
+local arr2=(${str2//$slpit_char2/ }) 
 
 # 路径-解析拼接
 #2 遍历某一数组
@@ -24,7 +24,6 @@ arr2=(${str2//$slpit_char2/ })
 #2 获取数组长度
 #2 获取数组下标
 #2 数组元素赋值
-
 for val2 in ${arr2[@]}  
 do   
     length=${#arr1[@]}
@@ -41,12 +40,38 @@ do
         #echo ${arr1[*]}
     fi
 done
-
 # 路径-转为字符
-str2=''
+local str3=''
 for i in ${arr1[@]};do
-  str2=$str2/$i;
+  str3=$str3/$i;
 done
- if [ -z $str2 ] ; then str2="/"; fi
-echo $str2
+if [ -z $str3 ] ; then str3="/"; fi
+echo $str3
+}
+function path_resolve(){
+local str1="${1}"
+local str2="${2}"
+local slpit_char1=/
+local slpit_char2=/
+if [[ -n ${3} ]]
+then
+    slpit_char1=${3}
+fi
+if [[ -n ${4} ]]
+then
+    slpit_char2=${4}
+fi
+
+#FIX:when passed asboult path,dose not return the asboult path itself
+#str2="/d/"
+local str3=""
+str2=$(echo $str2 | sed "s#/\$##")
+ABSOLUTE_PATH_REG_PATTERN="^/"
+if [[ $str2 =~ $ABSOLUTE_PATH_REG_PATTERN ]] ; 
+then 
+    str3=$str2; 
+else
+    str3=$(path_resolve_for_relative $str1 $str2 $slpit_char1 $slpit_char2)
+fi
+echo $str3
 }
